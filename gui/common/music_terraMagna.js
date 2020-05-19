0b0c5ab9225c27959bd8fa0e1add8bc3437de52c
KrimsonPrince <kanpal.kp1@gmail.com>
2020-05-19 10:00:09 -1000

    Commit message

---
+++ gui/common/music_terraMagna.js
@@ -0,0 +1,11 @@
+Music.prototype.resetTracks = function()
+ {
+ 	this.tracks = {
+		"MENU": ["MutantTheme.ogg"],
+		"PEACE": [],
+		"BATTLE": ["Taiko_1.ogg", "Taiko_2.ogg"],
+		"VICTORY": ["You_are_Victorious!.ogg"],
+		"DEFEAT": ["Omri Lahav - Red Dawn.ogg"],
+		"CUSTOM": []
+ 	};
+ };
